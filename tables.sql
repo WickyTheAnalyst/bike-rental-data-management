5579bd1e5c30958aafc87629f2a1bfaadff2098d
Waqas Ahmad <96549168+WickyTheAnalyst@users.noreply.github.com>
2023-10-09 02:01:10 +0500

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,64 +1,65 @@
-
-create table date_dim (
-	date_key integer PRIMARY KEY,
-	full_date date,
-	month integer,
-	day integer,
-	month_name varchar(20),
-	day_name varchar(20),
-	financial_qtr integer,
-	weekend boolean
+-- Create a table to store date-related information
+CREATE TABLE date_dim (
+    date_key integer PRIMARY KEY,     -- Primary key for date dimension
+    full_date date,                   -- Full date in 'YYYY-MM-DD' format
+    month integer,                    -- Month as a number (1-12)
+    day integer,                      -- Day of the month (1-31)
+    month_name varchar(20),           -- Month name (e.g., 'January')
+    day_name varchar(20),             -- Day name (e.g., 'Monday')
+    financial_qtr integer,            -- Financial quarter
+    weekend boolean                   -- Flag to indicate if it's a weekend day (TRUE/FALSE)
 );
 
-
-create table stations (
-	id integer PRIMARY KEY,
-	station_name varchar(50),
-	latitude real,
-	longitude real
+-- Create a table to store station information
+CREATE TABLE stations (
+    id integer PRIMARY KEY,           -- Primary key for stations
+    station_name varchar(50),         -- Name of the station
+    latitude real,                    -- Latitude coordinate
+    longitude real                    -- Longitude coordinate
 );
 
-create table trip_demo (
-	id integer PRIMARY KEY,
-	user_type varchar(50),
-	gender integer,
-	birth_year integer,
-	age integer
+-- Create a table to store trip demographic information
+CREATE TABLE trip_demo (
+    id integer PRIMARY KEY,           -- Primary key for trip demographics
+    user_type varchar(50),            -- User type (e.g., 'Subscriber', 'Customer')
+    gender integer,                   -- Gender (e.g., 1 for male, 2 for female)
+    birth_year integer,               -- Birth year of the user
+    age integer                       -- Age of the user
 );
 
-
-create table weather (
-	id serial PRIMARY KEY,
-	rec_date date,
-	avg_wind real,
-	prcp real,
-	snow_amt real,
-	snow_depth real,
-	tavg integer,
-	tmax integer,
-	tmin integer,
-	date_key integer,
-	rain boolean,
-	snow boolean,
-	FOREIGN KEY(date_key) REFERENCES date_dim(date_key)
+-- Create a table to store weather information
+CREATE TABLE weather (
+    id serial PRIMARY KEY,            -- Primary key for weather records (auto-increment)
+    rec_date date,                    -- Recorded date
+    avg_wind real,                    -- Average wind speed
+    prcp real,                        -- Precipitation
+    snow_amt real,                    -- Snowfall amount
+    snow_depth real,                  -- Snow depth
+    tavg integer,                     -- Average temperature
+    tmax integer,                     -- Maximum temperature
+    tmin integer,                     -- Minimum temperature
+    date_key integer,                 -- Foreign key to date dimension
+    rain boolean,                     -- Flag to indicate rain (TRUE/FALSE)
+    snow boolean,                     -- Flag to indicate snow (TRUE/FALSE)
+    FOREIGN KEY(date_key) REFERENCES date_dim(date_key)  -- Relationship to date dimension
 );
 
-
-create table rides (
-	id integer PRIMARY KEY,
-	date_key integer,
-	trip_duration integer,
-	trip_minutes real,
-	trip_hours real,
-	start_time timestamp,
-	stop_time timestamp,
-	start_station_id integer,
-	end_station_id integer,
-	bike_id integer,
-	valid_duration boolean,
-	trip_demo integer,
-	FOREIGN KEY(date_key) REFERENCES date_dim(date_key),
-	FOREIGN KEY(start_station_id) REFERENCES stations(id),
-	FOREIGN KEY(end_station_id) REFERENCES stations(id),
-	FOREIGN KEY(trip_demo) REFERENCES trip_demo(id)
+-- Create a table to store ride information
+CREATE TABLE rides (
+    id integer PRIMARY KEY,           -- Primary key for ride records
+    date_key integer,                 -- Foreign key to date dimension
+    trip_duration integer,            -- Trip duration in seconds
+    trip_minutes real,                -- Trip duration in minutes (decimal)
+    trip_hours real,                  -- Trip duration in hours (decimal)
+    start_time timestamp,             -- Start time of the trip
+    stop_time timestamp,              -- Stop time of the trip
+    start_station_id integer,         -- Start station ID (foreign key)
+    end_station_id integer,           -- End station ID (foreign key)
+    bike_id integer,                  -- Bike ID
+    valid_duration boolean,           -- Flag to indicate valid duration (TRUE/FALSE)
+    trip_demo integer,                -- Foreign key to trip demographics
+    FOREIGN KEY(date_key) REFERENCES date_dim(date_key),         -- Relationship to date dimension
+    FOREIGN KEY(start_station_id) REFERENCES stations(id),       -- Relationship to start station
+    FOREIGN KEY(end_station_id) REFERENCES stations(id),         -- Relationship to end station
+    FOREIGN KEY(trip_demo) REFERENCES trip_demo(id)              -- Relationship to trip demographics
 );
